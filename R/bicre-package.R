a3cefca34f02601773e9beb6d78638f194c9cf3c
Patrick Schnell <schnellp@gmail.com>
2024-12-11 13:56:41 -0500

    Imports necessary functions from other packages

--- R/bicre-package.R
+++ R/bicre-package.R
@@ -4,6 +4,22 @@
 NULL
 
 ## usethis namespace: start
+#' @import dplyr
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom data.table rbindlist
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom parallel mcmapply
+#' @importFrom parallel mclapply
 ## usethis namespace: end
 NULL
